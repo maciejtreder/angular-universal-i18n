d5a78c893ed07e949147cd2a18ef1f572d017616
Maciej Treder <contact@maciejtreder.com>
2018-12-05 16:19:38 +0100

    Angular Universal i18n - step 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID, Optional } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { REQUEST } from '@nguniversal/express-engine/tokens';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -9,19 +11,31 @@ import { TranslateService } from '@ngx-translate/core';
 export class AppComponent implements OnInit {
   title = 'angulari18n';
 
-  constructor(private translate: TranslateService) {}
+  constructor(
+    private translate: TranslateService,
+    @Optional() @Inject(REQUEST) private request: Request,
+    @Inject(PLATFORM_ID) private platformId: any
+  ) {}
 
   public ngOnInit(): void {
-    this.setDefaultTranslation();
-  }
-
-  private setDefaultTranslation(): void {
-    if (['en', 'es', 'zh', 'ru'].indexOf(this.translate.getBrowserLang()) > -1) {
-      this.translate.setDefaultLang(this.translate.getBrowserLang());
+    const language = this.getLang();
+    if (['en', 'es', 'zh', 'ru'].indexOf(language) > -1) {
+      this.translate.setDefaultLang(language);
     } else {
       this.translate.setDefaultLang('en');
     }
   }
+
+  public getLang(): string {
+    let lang: string;
+    if (isPlatformBrowser(this.platformId)) {
+      lang = this.translate.getBrowserLang();
+    } else {
+      lang = (this.request.headers['accept-language'] || '').substring(0, 2);
+    }
+    return lang;
+  }
+
 
   public switchLanguage(lang: string): void {
     this.translate.setDefaultLang(lang);
--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,24 +1,37 @@
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
-import {NgModule} from '@angular/core';
-    import {ServerModule, ServerTransferStateModule} from '@angular/platform-server';
-    import {ModuleMapLoaderModule} from '@nguniversal/module-map-ngfactory-loader';
-    import { BrowserModule } from '@angular/platform-browser';
-    import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-    
-    @NgModule({
- bootstrap: [AppComponent],
+import { NgModule } from '@angular/core';
+import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
+import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
+import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { Observable, Observer } from 'rxjs';
+import { readFileSync } from 'fs';
 
-        imports:[
- BrowserModule.withServerTransition({appId: 'app-root'}),
- 
- AppModule,
- 
-            ServerModule,
-            NoopAnimationsModule,
-            ModuleMapLoaderModule,
-            ServerTransferStateModule, // comment
-        ]
-    })
-    export class AppServerModule {}
-    
+export function universalLoader(): TranslateLoader {
+    return {
+        getTranslation: (lang: string) => {
+            return Observable.create((observer: Observer<any>) => {
+                observer.next(JSON.parse(readFileSync(`./dist/browser/assets/i18n/${lang}.json`, 'utf8')));
+                observer.complete();
+            });
+        }
+    } as TranslateLoader;
+}
+
+@NgModule({
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule.withServerTransition({appId: 'app-root'}),
+        AppModule,
+        ServerModule,
+        NoopAnimationsModule,
+        ModuleMapLoaderModule,
+        ServerTransferStateModule,
+        TranslateModule.forRoot({
+            loader: {provide: TranslateLoader, useFactory: universalLoader}
+        })
+    ]
+})
+export class AppServerModule {}
